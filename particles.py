cd0f06bb27a30a66c819c597261386b20d9238ca
blippard <blippard@gmail.com>
2021-05-15 13:55:05 +0200

    Method: added print_map to Map class

--- particles.py
+++ particles.py
@@ -3,3 +3,6 @@ class Map:
     def __init__ (self, width, height):
         self.map = [[" " for _ in range(width)] for _ in range(height)]
 
+    def print_map(self):
+        for row in range(len(self.map)):
+            print("".join(self.map[row])
