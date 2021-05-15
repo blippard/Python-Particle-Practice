07e22f68c62c7189902ac9ec78d356e6dbc00e6c
blippard <blippard@gmail.com>
2021-05-15 14:31:31 +0200

    Edit: refactored and fixed code

--- particles.py
+++ particles.py
@@ -7,17 +7,17 @@ bg_block = " "
 
 class Map:
 
-    def __init__ (self, width, height):
+    def __init__(self, width, height):
         self.map = [[bg_block for _ in range(width)] for _ in range(height)]
 
     def print_map(self):
         for row in range(len(self.map)):
-            print("".join(self.map[row])
+            print("".join(self.map[row]))
 
     def insert_obstacle(self, x_pos, y_pos, length):
         obstacle_length = x_pos + length
         for x_coord in range(x_pos, obstacle_length):
-            self.map[y_pos][x_coord] = "\033[37;47m*\1xb[0m"
+            self.map[y_pos][x_coord] = "\033[37;47m*\x1b[0m"
 
 class Particle:
 
@@ -39,12 +39,12 @@ class Particle:
             self.x_pos += 1
 
 class Game:
-
+    
     def __init__(self):
         self.game_map = Map(30, 30)
         self.particle_list = []
 
-    def generate_particle(self, icon):
+    def generate_particles(self, icon):
         self.particle_list.append(Particle(random.randint(5, 15), 0, icon))
 
     def position_particles(self):
@@ -52,7 +52,7 @@ class Game:
             self.game_map.map[particle.y_pos][particle.x_pos] = bg_block
             particle.move_to_available(self.game_map.map)
             self.game_map.map[particle.y_pos][particle.x_pos] = particle.icon
-
+    
     def generate_obstacles(self, amount):
         for _ in range(amount):
             x_pos = random.randint(0, 20)
@@ -61,7 +61,7 @@ class Game:
             self.game_map.insert_obstacle(x_pos, y_pos, length)
 
     def clear_screen(self):
-        os.system("cls" if os.name == "nt" else "clear")
+        os.system('cls' if os.name == 'nt' else 'clear')
 
     def refresh(self):
         self.position_particles()
@@ -69,7 +69,7 @@ class Game:
 
     def game_loop(self):
         while True:
-            self.generate_particles("█")
+            self.generate_particles("█") #•
             self.clear_screen()
             self.refresh()
             time.sleep(0.05)
@@ -78,5 +78,5 @@ class Game:
         self.generate_obstacles(5)
         self.game_loop()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     Game().run_game()
