8b5a476bd9ee5cb42021c8fc224b044ec321da72
blippard <blippard@gmail.com>
2021-05-15 13:57:39 +0200

    Add: method insert_obstacle in Map class

--- particles.py
+++ particles.py
@@ -6,3 +6,8 @@ class Map:
     def print_map(self):
         for row in range(len(self.map)):
             print("".join(self.map[row])
+
+    def insert_obstacle(self, x_pos, y_pos, length):
+        obstacle_length = x_pos + length
+        for x_coord in range(x_pos, obstacle_length):
+            self.map[y_pos][x_coord] = "\033[37;47m*\1xb[0m"
